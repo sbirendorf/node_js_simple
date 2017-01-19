48db85e59ff0528127c6c092268d4c4ea1e8c469
sbirendorf <sbirendorf@gmail.com>
2017-01-19 09:50:59 -0800

    first

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-var express = require('express')
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var express = require('express');
 var app = express();
-var request = require('request');           //Load the request module
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public
-app.use(bodyParser.urlencoded({'extended': 'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.set('port', (process.env.PORT || 5000));
 
- 
-app.post('/api/link', function (req, res) {
-     
-     var link = JSON.parse(req.body.data);
-     var url =  link.site;
-     console.log(url);
-     request({
-        uri: url,
-        method: "GET",
-        timeout: 10000,
-        followRedirect: true,
-        maxRedirects: 10,
-      }, function(error, response, body) {
-            if(!error){
-              res.send(body);
-            }else{
-               res.send(error); 
-            }
-      });
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
-app.listen(8080, function () {
-  console.log('listening on port 8080!');
-})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
